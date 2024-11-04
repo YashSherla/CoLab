2e3c237a27c702c5512ec43039ccf55016daf688
Nikhil Bura <nikhilburq70140@gmail.com>
2024-11-04 18:57:52 +0530

    Error Solved the file uploading  and remove

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const url = "mongodb://localhost:27017/coLab"; // yash laptop
-// const url = "mongodb+srv://admin:admin@cluster0.osm2m7k.mongodb.net/coLab" // office laptop
+// const url = "mongodb://localhost:27017/coLab"; // yash laptop
+const url = "mongodb+srv://admin:admin@cluster0.osm2m7k.mongodb.net/coLab" // office laptop
 const mogoConnect = async () => {
     try {
         let connect = await mongoose.connect(url);
